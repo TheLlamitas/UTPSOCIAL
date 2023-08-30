fde544db564487226330932dd4d2a160ed189bb4
EmanuelMayaP <emanuelmaya09@gmail.com>
2023-08-29 21:00:32 -0500

    config TailwindCSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
+        @vite('resources/css/app.css')
     </head>
     <body>
         <nav>
