28fbd6814c6248edcd5b1f2d5d4e733222f637be
EmanuelMayaP <emanuelmaya09@gmail.com>
2023-09-02 00:08:29 -0500

    clase 01 de sep

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,4 +9,11 @@ class RegisterController extends Controller
     public function index() {
         return view('auth.register');
     }
+
+    public function store()
+    {
+        dd('primer dd');
+        dump("sigo");
+        dd('Hola desde el controlador');
+    }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,5 +5,89 @@
 @endsection
 
 @section('contenido')
-    <p>Todo lo relacionado a la página principal</p>
+    <div class="md:flex md:justify-center md:items-center">
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-2xl">
+            <div class="md:flex md:justify-center hidden sm:block">
+                <img class="max-h-20" src="{{ asset('img/utp.png') }}" alt="UTP Social">
+            </div>
+            <h2 class="mb-5 text-gray-600 font-bold font-sans text-center">
+                Registrate para ver fotos y vídeos de tus amigos.
+            </h2>
+            <hr class="mb-5">
+
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Username"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Correo electronico"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Contraseña
+                    </label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Tu Contraseña"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Reprtir Contraseña
+                    </label>
+                    <input
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Repite tu Contraseña"
+                        class="border p-3 w-full rounded-lg"
+                    >
+                </div>
+                <input
+                    type="submit"
+                    value="Crear Cuenta"
+                    class=" bg-sky-600 hover:bg-green-600 transition-colors cursor-pointer uppercase w-full p-3 text-white rounded-lg font-bold"
+                />
+            </form>
+        </div>
+    </div>
+    <div class="md:flex md:justify-center md:items-center mt-10">
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-2xl">
+            <h2 class="text-center">¿Tienes una cuenta? <a class=" text-blue-600" href="/login">Inicia sesion</a></h2>
+        </div>
+    </div>
 @endsection
